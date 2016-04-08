89ef8d0b3e7913d40cdf071141cef448056e4585
Mocanu Valentin <mr_vali@yahoo.ro>
2016-04-08 15:54:36 +0300

    Modified the code so it won't crash on windows (also moved rpi-gpio to optionalDependencies)

--- RadioPi/index.js
+++ RadioPi/index.js
@@ -1,45 +1,57 @@
 const speakerPin = 11;
-const gpio = require('rpi-gpio');
 const fs = require('fs');
 const lirc = require('lirc_node');
 const io = require('socket.io-client');
 const Minilog = require('minilog');
-Minilog.pipe(Minilog.backends.console.formatColor).pipe(Minilog.backends.console);
+Minilog.pipe(Minilog.backends.console.formatMinilog).pipe(Minilog.backends.console);
 const log = Minilog('RadioPi \t');
 const MPlayer = require('mplayer');
-const player = new MPlayer();
 
-const dir = '/home/radiopi';
+var gpio, player;
+try {
+    gpio = require('rpi-gpio');
+    player = new MPlayer();
+} 
+catch (er) {
+    gpio = null;
+}
 
+const dir = process.env.windir ? './RadioPi' : '/home/radiopi';
 const tvRemote = true;
+
+var stations = JSON.parse(fs.readFileSync(dir + '/radioStations.json'));
+
+var state = {
+    lastPlayed: 0,
+    playing: false,
+    volume: 100
+};
+var restart = false;
+
 
 var socket = io.connect('http://localhost', {
     reconnect: true,
     reconnectionDelayMax: 1000
 });
 socket.on('connect', () => {
-    log.debug('connected');
+    log.debug('Connected to homeControl');
     socket.emit('setServerName', 'Radio');
 });
 
-gpio.setup(speakerPin, gpio.DIR_OUT, () => {
-    var stations = JSON.parse(fs.readFileSync(dir + '/radioStations.json'));
-
-    var state = {
-        lastPlayed: 0,
-        playing: false,
-        volume: 100
-    };
-    var restart = false;
-
-    player.on('stop', () => {
-        state.playing = false;
-        log.info(Date.now(), 'Mplayer stopped', restart);
-        if (!restart) {
-            updateGPIO (speakerPin, false);
-            socket.emit('Radio:state', state);
-        }
-    });
+if (gpio) {
+    gpio.setup(speakerPin, gpio.DIR_OUT, startup);
+}
+function startup () {
+    if (player) {
+        player.on('stop', () => {
+            state.playing = false;
+            log.info(Date.now(), 'Mplayer stopped', restart);
+            if (!restart) {
+                updateGPIO (speakerPin, false);
+                socket.emit('Radio:state', state);
+            }
+        });
+    }
 
     fs.watch(dir + '/radioStations.json', () => {
         var newFile = fs.readFileSync(dir + '/radioStations.json');
@@ -95,69 +107,75 @@ gpio.setup(speakerPin, gpio.DIR_OUT, () => {
         socket.emit('Radio:stations', stations);
     });
 
-    function updateGPIO (pin, state, callback) {
-        if (typeof callback != 'function') callback = function () {};
+    updateGPIO(speakerPin, false);
+    log.info('Startup completed');
+}
 
-        gpio.write(pin, state, callback);
+function updateGPIO (pin, state, callback) {
+    if (gpio && typeof callback != 'function') callback = function () {};
+
+    gpio.write(pin, state, callback);
+}
+
+function nextStation () {
+    state.lastPlayed++;
+    state.lastPlayed %= stations.length;
+    return stations[state.lastPlayed];
+}
+function previousStation () {
+    state.lastPlayed--;
+    if (state.lastPlayed < 0) state.lastPlayed = stations.length + state.lastPlayed;
+    return stations[state.lastPlayed];
+}
+
+function startRadio (station) {
+    var url = station;
+    if (!station) url = stations[state.lastPlayed];
+    station = parseInt(station);
+    if (station >= 0 && station < stations.length) url = stations[station];
+
+    if (!url || !url.stream) {
+        url = stations[0];
+        log.warning (Date.now(), 'Start radio received an empty station url');
     }
+    log.info(Date.now(), 'Start radio', url.name);
+    station = url;
+    url = url.stream;
 
-    function nextStation () {
-        state.lastPlayed++;
-        state.lastPlayed %= stations.length;
-        return stations[state.lastPlayed];
-    }
-    function previousStation () {
-        state.lastPlayed--;
-        if (state.lastPlayed < 0) state.lastPlayed = stations.length + state.lastPlayed;
-        return stations[state.lastPlayed];
-    }
-
-    function startRadio (station) {
-        var url = station;
-        if (!station) url = stations[state.lastPlayed];
-        station = parseInt(station);
-        if (station >= 0 && station < stations.length) url = stations[station];
-
-        if (!url || !url.stream) {
-            url = stations[0];
-            log.warning (Date.now(), 'Start radio received an empty station url');
-        }
-        log.info(Date.now(), 'Start radio', url.name);
-        station = url;
-        url = url.stream;
-
-        restart = true;
+    restart = true;
+    if (player) {
         player.openFile(url);
         player.play();
-        updateGPIO(speakerPin, true);
-
-        state.playing = true;
-        state.lastPlayed = stations.indexOf(station);
-
-        socket.emit('Radio:state', state);
     }
+    updateGPIO(speakerPin, true);
 
-    function stopRadio () {
-        restart = false;
+    state.playing = true;
+    state.lastPlayed = stations.indexOf(station);
+
+    socket.emit('Radio:state', state);
+}
+
+function stopRadio () {
+    restart = false;
+    if (player) {
         player.stop();
     }
+}
 
-    function toggleRadio () {
-        restart ? stopRadio() : startRadio();
+function toggleRadio () {
+    restart ? stopRadio() : startRadio();
+}
+
+function changeVolume (mode) {
+    if (typeof mode == 'string') {
+        state.volume += 5 * (mode == '+' ? 1 : -1);
     }
-
-    function changeVolume (mode) {
-        if (typeof mode == 'string') {
-            state.volume += 5 * (mode == '+' ? 1 : -1);
-        }
-        else if (typeof mode == 'number') {
-            state.volume = mode;
-        }
-        state.volume = Math.min(Math.max(state.volume, 0), 100);
-        log.debug('New volume', state.volume);
+    else if (typeof mode == 'number') {
+        state.volume = mode;
+    }
+    state.volume = Math.min(Math.max(state.volume, 0), 100);
+    log.debug('New volume', state.volume);
+    if (player) {
         player.volume(state.volume);
     }
-
-    updateGPIO(speakerPin, false);
-    log.info(Date.now(), 'Startup completed');
-});
+}
