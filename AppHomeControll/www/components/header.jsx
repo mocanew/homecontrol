9f8ba5758436d1024f869d9ee19c4c88a2c3f3d5
Mocanu Valentin <mr_vali@yahoo.ro>
2016-04-19 22:16:07 +0300

    Complete rewrite of the app

---
+++ AppHomeControll/www/components/header.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import MaterialButton from './materialButton.jsx';
+import '../scss/common.scss';
+import '../scss/header.scss';
+import classNames from 'classnames';
+import { Link } from 'react-router';
+
+class Header extends React.Component {
+    constructor (props) {
+        super(props);
+        this.toggleMenu = this.toggleMenu.bind(this);
+        this.handleResize = this.handleResize.bind(this);
+        this.state = {
+            menu: false,
+            collapse: true
+        };
+    }
+    toggleMenu () {
+        this.setState({
+            menu: !this.state.menu
+        });
+    }
+    handleResize () {
+        if (window.innerWidth > 992) {
+            this.setState({
+                collapse: false
+            });
+        }
+        else {
+            this.setState({
+                collapse: true
+            });
+        }
+    }
+    componentDidMount () {
+        window.addEventListener('resize', this.handleResize);
+        this.handleResize();
+    }
+    componentWillUnmount () {
+        window.removeEventListener('resize', this.handleResize);
+    }
+    render () {
+        var classes = classNames({
+            menuWrapper: true,
+            big: !this.state.collapse,
+            open: this.state.collapse && this.state.menu
+        });
+
+        return (
+            <div className={classes}>
+                <nav className="vertical">
+                    <Link to="/" className="logo">
+                        Home Control
+                    </Link>
+                    <div className="links">
+                        <Link to="/radiopi">Radio Pi</Link>
+                        <Link to="/wakeonlan">Wake on Lan</Link>
+                    </div>
+                </nav>
+                <nav className="horizontal">
+                    <MaterialButton buttonStyle="flat iconBtn menu" classes="hamburger" onClick={this.toggleMenu}></MaterialButton>
+                    <div className="pageTitle">
+                        {this.props.documentTitle}
+                    </div>
+                </nav>
+                <div className="backdrop" onClick={this.toggleMenu}></div>
+            </div>
+        );
+    }
+}
+Header.propTypes = {
+    documentTitle: React.PropTypes.string
+};
+
+export default Header;
---
+++ AppHomeControll/www/routes/wakeonlan.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class WakeOnLan extends React.Component {
+    constructor (props) {
+        super (props);
+    }
+    render () {
+        document.title = 'Home Control';
+        return (
+            <div>wakeonlan</div>
+        );
+    }
+}
+
+export default WakeOnLan;
