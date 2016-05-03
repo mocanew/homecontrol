f6977636879542f8f670b45cdb71953f1f11f18b
Mocanu Valentin <mr_vali@yahoo.ro>
2016-05-03 15:55:18 +0300

    Deployment script for Snap CI

---
+++ sftp.js
@@ -0,0 +1,53 @@
+var files = ['homeControl.js', 'package.json', 'LICENSE'];
+var dirs = ['RadioPi', 'WakeOnLan', 'build'];
+const async = require('async');
+
+var conf = {
+    host: 'rontav.go.ro',
+    username: '',
+    password: '',
+    _HomePath: '/home/node/homecontrol/'
+};
+
+async.waterfall([
+    callback => {
+        var Client = require('scp2').Client;
+        var client = new Client(conf);
+
+        async.eachSeries(files, (file, cb) => client.upload(file, conf._HomePath, cb),
+            (err) => {
+                console.log(err ? err : 'Files uploaded');
+                callback(err);
+            });
+    },
+    callback => {
+        var client = require('scp2');
+
+        async.eachSeries(dirs, (dir, cb) => client.scp(dir, conf.username + ':' + conf.password + '@' + conf.host + ':' + conf._HomePath + dir, cb),
+            (err) => {
+                console.log(err ? err : 'Folders uploaded');
+                callback(err);
+            });
+    },
+    callback => {
+        var SSH = require('simple-ssh');
+
+        var ssh = new SSH({
+            host: conf.host,
+            user: conf.username,
+            pass: conf.password,
+            baseDir: conf._HomePath
+        });
+        ssh.exec('ls ./ && npm install --production && service wakeonlan restart && service radiopi restart && service homeControl restart', {
+            out: console.log.bind(console),
+            err: console.log.bind(console),
+            exit: (code) => {
+                console.log(code);
+                callback();
+            }
+        }).start();
+    }
+], (err) => {
+    console.log(err ? err : 'Done');
+    process.exit();
+})
