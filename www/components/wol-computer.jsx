88c75de4f572c0dcdb1675c73039d8c63a1da63d
Mocanu Valentin <mr_vali@yahoo.ro>
2016-05-15 20:57:23 +0300

    Added remove card icon, moved the power dot to the left side of the card. Added a card where you can add/ping/wake a new computer.

--- www/components/wol-computer.jsx
+++ www/components/wol-computer.jsx
@@ -1,52 +1,103 @@
 import React from 'react';
 import MaterialButton from './materialButton.jsx';
-import '../scss/_card.scss';
+import Input from './input.jsx';
 import classNames from 'classnames';
 
 class Card extends React.Component {
-    constructor (props) {
+    constructor(props) {
         super(props);
         this.state = {
-            power: this.props.power
+            power: this.props.power,
+            ip: this.props.ip,
+            mac: this.props.mac
         };
         this.wake = this.wake.bind(this);
         this.ping = this.ping.bind(this);
-        
+        this.save = this.save.bind(this);
+
         this.name = this.props.name ? this.props.name : 'Computer ' + this.props.id;
     }
-    static get defaultProps () {
+    static get defaultProps() {
         return {
             power: false,
             name: '',
             ip: '',
             mac: '',
-            image: 'images/general.jpg'
+            image: '/images/general.jpg'
         };
     }
-    componentWillReceiveProps (nextProps) {
+    componentWillReceiveProps(nextProps) {
+        if (!nextProps.direct && nextProps.power == this.props.power) return;
+
         this.setState({
             power: nextProps.power
         });
     }
-    wake () {
+    wake() {
         this.setState({
             power: ''
         });
         socket.emit('WakeOnLan', {
-            ip: this.props.ip,
-            mac: this.props.mac
+            ip: this.state.ip,
+            mac: this.state.mac
         });
     }
-    ping () {
+    ping() {
         this.setState({
             power: ''
         });
         socket.emit('WakeOnLan:ping', {
-            ip: this.props.ip,
-            mac: this.props.mac
+            ip: this.state.ip,
+            mac: this.state.mac
         });
     }
-    render () {
+    save() {
+        var error = this.refs.ip.error || this.refs.mac.error;
+        if (this.refs.ip.value.trim().length <= 0 && this.refs.mac.value.trim().length <= 0) {
+            var temp = {
+                error: true
+            };
+            error = true;
+            this.refs.ip.setState(temp);
+            this.refs.mac.setState(temp);
+        }
+        this.refs.ip.state.error ? this.refs.ip.highlightError() : true;
+        this.refs.mac.state.error ? this.refs.mac.highlightError() : true;
+
+        if (error) return;
+
+        this.props.onSave({
+            name: this.refs.name.value,
+            ip: this.refs.ip.value,
+            mac: this.refs.mac.value
+        });
+    }
+    verifyIP(e) {
+        console.log(e);
+        return false;
+    }
+    verifyMAC(e) {
+        console.log(e);
+        return false;
+    }
+    render() {
+        if (typeof this.props.onSave == 'function') {
+            return (
+                <div className="wolCard new">
+                    <div className="wrapper">
+                        <Input ref="name">Computer name</Input>
+                        <Input validator={this.verifyIP} message="Introduceți o adresă IP validă" ref="ip">IP</Input>
+                        <Input validator={this.verifyMAC} message="Introduceți o adresă MAC validă" ref="mac">MAC</Input>
+                        <div className="buttonsRow">
+                            <MaterialButton buttonStyle="flat" onClick={this.save}>Add</MaterialButton>
+                            <MaterialButton buttonStyle="flat" onClick={this.ping}>Ping</MaterialButton>
+                            <MaterialButton buttonStyle="flat" onClick={this.wake}>Wake</MaterialButton>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+
         var power = classNames({
             power: true,
             pinging: typeof this.state.power != 'boolean' ? true : false,
@@ -56,36 +107,33 @@ class Card extends React.Component {
             backgroundImage: 'url("' + this.props.image + '")'
         };
         return (
-            <div className="card col-xs-12 col-sm-4 col-md-4">
-            <div className="wrapper">
-                <div className={power}>{ typeof this.state.power != 'boolean' ? <i className="fa fa-refresh fa-spin fa-fw"></i> : ''}</div>
-                <div className="image" style={imageStyle}></div>
-                <div className="titleRow">
-                    <div className="title">{this.name}</div>
-                    <div className="subtitle">{this.props.ip && this.props.ip.length ? (this.props.mac && this.props.mac.length ? this.props.ip + ' - ' + this.props.mac : this.props.ip) : this.props.mac}</div>
+            <div className="wolCard">
+                <div className="wrapper">
+                    <div title="Șterge" className="trash" onClick={this.props.remove(this.props) }><i className="fa fa-trash-o fa-fw"></i></div>
+                    <div title={this.state.power === true ? 'Pornit' : this.state.power === false ? 'Oprit' : ''} className={power} onClick={this.ping}>{ typeof this.state.power != 'boolean' ? <i className="fa fa-refresh fa-spin fa-fw"></i> : ''}</div>
+                    <div className="image" style={imageStyle}></div>
+                    <div className="titleRow">
+                        <div className="title">{this.name}</div>
+                        <div className="subtitle">{this.props.ip && this.props.ip.length ? (this.props.mac && this.props.mac.length ? this.props.ip + ' - ' + this.props.mac : this.props.ip) : this.props.mac}</div>
+                    </div>
+                    <div className="buttonsRow">
+                        <MaterialButton buttonStyle="flat" onClick={this.ping}>Ping</MaterialButton>
+                        <MaterialButton buttonStyle="flat" onClick={this.wake}>Wake</MaterialButton>
+                    </div>
                 </div>
-                <div className="buttonsRow">
-                    <MaterialButton buttonStyle="flat" onClick={this.ping}>Ping</MaterialButton>
-                    <MaterialButton buttonStyle="flat" onClick={this.wake}>Wake</MaterialButton>
-                </div>
-            </div>
             </div>
         );
     }
 }
 Card.propTypes = {
-    id: React.PropTypes.oneOfType([
-        React.PropTypes.string,
-        React.PropTypes.number
-    ]),
+    id: React.PropTypes.number,
     name: React.PropTypes.string,
     ip: React.PropTypes.string,
     mac: React.PropTypes.string,
     image: React.PropTypes.string,
-    power: React.PropTypes.oneOfType([
-        React.PropTypes.string,
-        React.PropTypes.boolean
-    ])
+    power: React.PropTypes.bool,
+    onSave: React.PropTypes.func,
+    remove: React.PropTypes.func
 };
 
 export default Card;
--- www/routes/wakeonlan.jsx
+++ www/routes/wakeonlan.jsx
@@ -3,35 +3,51 @@ import Card from '../components/wol-computer.jsx';
 import '../scss/wakeonlan.scss';
 
 class WakeOnLan extends React.Component {
-    constructor (props) {
-        super (props);
+    constructor(props) {
+        super(props);
         this.state = {
             cards: this.props.cards
         };
         this.response = this.response.bind(this);
+        this.addCard = this.addCard.bind(this);
+        this.remove = this.remove.bind(this);
     }
-    response (e) {
+    addCard(e) {
+        var temp = this.state.cards;
+        temp.push(e);
+        this.setState({
+            cards: temp
+        });
+    }
+    response(e) {
         if (typeof e != 'object') return;
-        
+
         if (e.length) {
             for (var i = 0; i < e.length; i++) {
                 this.reponse(e[i]);
             }
             return;
         }
-        
+
+        e.ip = e.ip && e.ip.length ? e.ip : undefined;
+        e.mac = e.mac && e.mac.length ? e.mac : undefined;
+
         console.log('Wol response', e);
         var temp = this.state.cards;
-        
+
         for (var w = 0; w < temp.length; w++) {
-            if (temp[w].ip != e.ip && temp[w].mac != e.mac) continue;
+            if (temp[w].ip != e.ip && temp[w].mac != e.mac) {
+                temp[w].direct = false;
+                continue;
+            }
             temp[w].power = e.isAlive;
+            temp[w].direct = true;
         }
         this.setState({
             cards: temp
         });
     }
-    error (e) {
+    error(e) {
         console.error(e);
     }
     componentDidMount() {
@@ -44,31 +60,48 @@ class WakeOnLan extends React.Component {
     }
     static get defaultProps() {
         return {
-            cards: [{
-                ip: '192.168.1.1',
-                mac: 'FF:FF:FF:FF:FF:F1',
-                image: '../images/win7.jpg'
-            },
-            {
-                ip: '192.168.1.115',
-                image: '../images/win7.jpg'
-            },
-            {
-                ip: '192.168.1.104',
-                mac: 'FF:FF:FF:FF:FF:F2',
-                image: '../images/win7.jpg'
-            }]
+            cards: [
+                {
+                    ip: '192.168.0.1',
+                    mac: 'FF:FF:FF:FF:FF:F1',
+                    image: 'http://localhost:8090/www/images/win7.jpg'
+                },
+                {
+                    ip: '192.168.1.115',
+                    image: 'http://localhost:8090/www/images/linux.png'
+                },
+                {
+                    ip: '192.168.0.102',
+                    mac: 'FF:FF:FF:FF:FF:F2',
+                    image: 'http://localhost:8090/www/images/win7.jpg'
+                }
+            ]
         };
     }
-    render () {
+    remove(e) {
+        return function () {
+            console.log(e);
+            var temp = this.state.cards;
+
+            for (var w = 0; w < temp.length; w++) {
+                if (temp[w].ip != e.ip && temp[w].mac != e.mac) continue;
+                temp.splice(w, 1);
+            }
+            this.setState({
+                cards: temp
+            });
+        }.bind(this);
+    }
+    render() {
         document.title = 'Home Control';
         return (
             <div className="wakeonlan">
                 {
                     this.state.cards.map((e, i) => {
-                        return <Card power={e.power} name={e.name} ip={e.ip} mac={e.mac} image={e.image} id={i} key={'card' + i} />;
+                        return <Card remove={this.remove} direct={e.direct} power={e.power} name={e.name} ip={e.ip} mac={e.mac} image={e.image} id={i + 1} key={'card' + i} />;
                     })
                 }
+                <Card onSave={this.addCard} />
             </div>
         );
     }
