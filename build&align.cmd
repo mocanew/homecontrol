ef826b7c7f86ba2e5f3b7987e3723f8bb2c62148
Mocanu Valentin <mr_vali@yahoo.ro>
2016-05-15 13:12:06 +0300

    I will move this functionality to build.js

--- build&align.cmd
+++
@@ -1,4 +0,0 @@
-cd AppHomeControll
-call cordova build android --release
-cd ..
-"F:\3D\ADT Bundle\sdk\build-tools\23.0.1\zipalign.exe" -f 4 "AppHomeControll\platforms\android\build\outputs\apk\android-release-unsigned.apk" radiopi.apk
