2ef67aa6065b4f2957c948d76ebcb3f83b9dd8aa
Mocanu Valentin <mr_vali@yahoo.ro>
2016-05-20 15:56:50 +0300

    Required is supposed to be a bool

--- www/components/stories/input.js
+++ www/components/stories/input.js
@@ -9,13 +9,13 @@ import 'bootstrap-webpack';
 storiesOf('Input', module)
     .add('Required & not required', () => (
         <div>
-            <Input required="true" onChange={action('Change name 1')}>Name 1</Input>
-            <Input required="false" onChange={action('Change name 2')}>Name 2</Input>
+            <Input required={true} onChange={action('Change name 1')}>Name 1</Input>
+            <Input required={false} onChange={action('Change name 2')}>Name 2</Input>
         </div>
     ))
     .add('Child vs no child', () => (
         <div>
-            <Input required="true" message="Custom error message" onChange={action('Change name 1')}>Name 1</Input>
-            <Input required="false" title="Name 2" name="email" onChange={action('Change name 2')}></Input>
+            <Input required={true} message="Custom error message" onChange={action('Change name 1')}>Name 1</Input>
+            <Input required={false} title="Name 2" name="email" onChange={action('Change name 2')}></Input>
         </div>
     ));
