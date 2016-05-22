c0c79562fd5f3bfb88a6cb9a7e010205560b1b32
Mocanu Valentin <mr_vali@yahoo.ro>
2016-05-22 22:00:39 +0300

    Added build for android feature in build.js (node build --android)

--- build.js
+++ build.js
@@ -1,5 +1,6 @@
 const async = require('async');
 const spawn = require('child_process').spawn;
+const exec = require('child_process').exec;
 const rimraf = require('rimraf');
 
 var colors;
@@ -31,9 +32,36 @@ async.waterfall([
         webpack.on('exit', () => cb());
     },
     cb => {
+        if (process.argv.indexOf('--android') == -1) return cb();
+
         log('--- Copy assets folder to AppHomeControll/www/assets ---', 'yellow');
         var copy = require('copy');
-        copy('./assets/*.*', './AppHomeControll/www/assets/', cb);
+        copy('./assets/*.*', './AppHomeControll/www/assets/', () => cb());
+    },
+    cb => {
+        if (process.argv.indexOf('--android') == -1) return cb();
+
+        log('--- Build Android app ---', 'yellow');
+        var cordova = spawn(process.env.windir ? 'cordova.cmd' : 'cordova', ['build', '--release', 'android'], {
+            cwd: './AppHomeControll'
+        });
+
+        cordova.stdout.on('data', data => process.stdout.write(data));
+        cordova.stderr.on('data', data => process.stdout.write(data));
+
+        cordova.on('exit', () => cb());
+    },
+    cb => {
+        if (process.argv.indexOf('--android') == -1) return cb();
+
+        log('--- Sign APK ---', 'yellow');
+        exec('jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore Radiopi.keystore -storepass rontavstudio ./AppHomeControll/platforms/android/build/outputs/apk/android-release-unsigned.apk Radiopi', () => cb());
+    },
+    cb => {
+        if (process.argv.indexOf('--android') == -1) return cb();
+
+        log('--- Align APK ---', 'yellow');
+        exec('zipalign -f -v 4 ./AppHomeControll/platforms/android/build/outputs/apk/android-release-unsigned.apk ./radiopi.apk', () => cb());
     }
     // cb => {
     //     log('--- NPM Shrinkwrap ---', 'yellow');
