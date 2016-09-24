3daeabe2855f7c3c819b0d5c8490c913af4bba5b
Valentin Mocanu <mr_vali@yahoo.ro>
2016-09-24 18:08:34 +0300

    The build task will now put all files into a zip and make a github release from wecker.

--- build.js
+++ build.js
@@ -1,15 +1,31 @@
 const async = require('async');
+const _ = require('lodash');
 const spawn = require('child_process').spawn;
 const exec = require('child_process').exec;
 const rimraf = require('rimraf');
 
 var colors;
+var zip = {
+    files: [
+        'homeControl.js',
+        'package.json',
+        'LICENSE',
+        'www/index.html'
+    ],
+    dirs: [
+        'models',
+        'routes',
+        'servers',
+        'assets',
+        'www/images'
+    ]
+};
 
 try {
     colors = require('colors');
 }
 catch (e) {
-    // eslint doesn't allow empty functions :D
+    console.warn('Colors is not installed. I\'m ugly :\'(');
 }
 
 function log(e, color) {
@@ -18,11 +34,23 @@ function log(e, color) {
     }
     console.log(e);
 }
+function isMode(mode) {
+    return process.argv.indexOf(mode) != -1 || process.argv.indexOf('--' + mode) != -1;
+}
 
 async.waterfall([
-    cb => rimraf('./assets/', {}, cb),
     cb => {
-        if (process.argv.indexOf('--webpack') == -1) return cb();
+        if (!isMode('webpack')) return cb();
+
+        rimraf('./assets/', {}, cb);
+    },
+    cb => {
+        if (!isMode('zip')) return cb();
+
+        rimraf('./build.zip', {}, cb);
+    },
+    cb => {
+        if (!isMode('webpack')) return cb();
 
         log('--- Webpack Build ---', 'yellow');
         var webpack = spawn(process.env.windir ? 'webpack.cmd' : 'webpack', ['--production', '--progress', '--optimize-minimize', '--optimize-dedupe', '--colors']);
@@ -34,14 +62,14 @@ async.waterfall([
         webpack.on('exit', () => cb());
     },
     cb => {
-        if (process.argv.indexOf('--android') == -1) return cb();
+        if (!isMode('android')) return cb();
 
         log('--- Copy assets folder to AppHomeControll/www/assets ---', 'yellow');
         var copy = require('copy');
         copy('./assets/*.*', './AppHomeControll/www/assets/', () => cb());
     },
     cb => {
-        if (process.argv.indexOf('--android') == -1) return cb();
+        if (!isMode('android')) return cb();
 
         log('--- Build Android app ---', 'yellow');
         var cordova = spawn(process.env.windir ? 'cordova.cmd' : 'cordova', ['build', '--release', 'android'], {
@@ -54,19 +82,19 @@ async.waterfall([
         cordova.on('exit', () => cb());
     },
     cb => {
-        if (process.argv.indexOf('--android') == -1) return cb();
+        if (!isMode('android')) return cb();
 
         log('--- Sign APK ---', 'yellow');
         exec('jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore Radiopi.keystore -storepass rontavstudio ./AppHomeControll/platforms/android/build/outputs/apk/android-release-unsigned.apk Radiopi', () => cb());
     },
     cb => {
-        if (process.argv.indexOf('--android') == -1) return cb();
+        if (!isMode('android')) return cb();
 
         log('--- Align APK ---', 'yellow');
         exec('zipalign -f -v 4 ./AppHomeControll/platforms/android/build/outputs/apk/android-release-unsigned.apk ./radiopi.apk', () => cb());
     },
     cb => {
-        if (process.argv.indexOf('--install') == -1) return cb();
+        if (!isMode('install')) return cb();
 
         log('--- Install apk ---', 'yellow');
         var called = false;
@@ -89,8 +117,30 @@ async.waterfall([
         });
     },
     cb => {
-        if (process.argv.indexOf('--install') == -1) return cb();
+        if (!isMode('install')) return cb();
 
         exec('adb shell am start -a android.intent.action.MAIN -n com.rontavstudio.radiopi/.MainActivity', () => cb());
+    },
+    cb => {
+        if (!isMode('zip')) return cb();
+
+        log('--- Archive build ---', 'yellow');
+        var AdmZip = require('adm-zip');
+
+        var zipFile = new AdmZip();
+
+        _.each(zip.files, (file) => {
+            zipFile.addLocalFile(file, file.lastIndexOf('/') != -1 ? file.substr(0, file.lastIndexOf('/')) : '');
+        });
+        _.each(zip.dirs, (dir) => {
+            zipFile.addLocalFolder(dir, dir);
+        });
+        zipFile.writeZip('./build.zip');
+        cb();
+    },
+    cb => {
+        if (!isMode('release')) return cb();
+
+        exec('export GITHUB_RELEASE_VERSION=' + require('./package.json').version, () => cb());
     }
 ], (e) => log(e ? e : '--- Build finished ---', e ? 'red' : 'green'));
